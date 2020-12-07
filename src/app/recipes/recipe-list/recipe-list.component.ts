8f556766ddf34743180466176cb0a9025b19a10a
Zak Rahman <zakriyarahman@gmail.com>
2020-12-07 07:05:53 -0600

    Add receipes and recipe-list and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
-        new Recipe('A Test recipe', 'This is a test', 'https://p0.pikist.com/photos/943/665/kyllingefrikadeller-potatoes-meatballs-recipes-food-pictures-recipe-kyllingefrikadelle.jpg')
+        new Recipe('A Test recipe', 'This is a test', 'https://p0.pikist.com/photos/943/665/kyllingefrikadeller-potatoes-meatballs-recipes-food-pictures-recipe-kyllingefrikadelle.jpg'),
+        new Recipe('Another Test recipe', 'This is a test', 'https://p0.pikist.com/photos/943/665/kyllingefrikadeller-potatoes-meatballs-recipes-food-pictures-recipe-kyllingefrikadelle.jpg')
     ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
